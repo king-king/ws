9b428075fe1c41eedac5a28dd93c9068c8d72245
qunwang <niuyiwangqun@sohu.com>
2015-05-30 00:06:45 +0800

    c

--- server/server.js
+++ server/server.js
@@ -3,23 +3,21 @@
  */
 var http = require( 'http' );
 var httpServer = http.createServer( function ( req, res ) {
-    console.log( "http connect" );
-    res.writeHead( 200, {'Content-Type' : 'text/plain'} );
-    res.end( 'var a=23' );
+	console.log( "http connect" );
+	res.writeHead( 200, {'Content-Type' : 'text/plain'} );
+	res.end( 'var a=23' );
 } );
 httpServer.listen( 8383 );
 
 var WebSocketServer = require( 'ws' ).Server,
-    wss = new WebSocketServer( {
-        server : httpServer
-    } );
+	wss = new WebSocketServer( {
+		server : httpServer
+	} );
 
 wss.on( 'connection', function connection( ws ) {
-    console.log( "ws connect" );
-
-    ws.on( 'message', function incoming( message ) {
-        console.log( 'received: %s', message );
-    } );
-
-    ws.send( 'something' );
+	console.log( "ws connect" );
+	ws.on( 'message', function incoming( message ) {
+		console.log( 'received: %s', message );
+	} );
+	ws.send( 'something' );
 } );
